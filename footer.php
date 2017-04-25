8710c909bcf0868af5b65a453e22c8b133a994d7
Jim Smith <jsxmedia@gmail.com>
2017-04-25 01:51:26 -0700

    Added necessary files for SASS support.

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jgm2018' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'jgm2018' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'jgm2018' ), 'jgm2018', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'jgm2018' ), 'jgm2018', '<a href="https://automattic.com/" rel="designer">Jim Smith</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
