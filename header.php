b8fc9ad105351e402ece7816876ab4ad87f8ed48
jsxmedia <jsxmedia@gmail.com>
2017-05-12 00:11:59 -0700

    Mobile: Removed "JG&m Expo" place holder text in top header. Styled menu button. Now takes adquately fills top header. Added hamburger icon menu button.
    All:
    Styled link colors.

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
             </div><!-- .brand-wrapper -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jgm2018' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'jgm2018' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
